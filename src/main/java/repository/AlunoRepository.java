f99359b4c5c453e1917f3e3386ffd9a3593e0d76
mariaeduardams <meduardamaiasantos@gmail.com>
2026-03-21 00:29:41 -0300

    adicionei persistência com H2 e repository com o banco (testes funcionando)

--- src/main/java/repository/AlunoRepository.java
+++ src/main/java/repository/AlunoRepository.java
@@ -1,18 +1,81 @@
 package repository;
 
-import java.util.ArrayList;
 import model.Aluno;
+import config.DatabaseConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AlunoRepository {
-    private ArrayList<Aluno> alunos =  new ArrayList<>();
-    public void cadastrar (Aluno aluno) {
-        alunos.add(aluno);
+
+    public AlunoRepository() {
+        criarTabela();
     }
 
-    public ArrayList<Aluno> listar() {
+    private void criarTabela() {
+        try (Connection conn = DatabaseConnection.getConnection();
+             Statement stmt = conn.createStatement()) {
+
+            stmt.execute("CREATE TABLE IF NOT EXISTS aluno (" +
+                    "id IDENTITY PRIMARY KEY, " +
+                    "nome VARCHAR(100), " +
+                    "cpf VARCHAR(11) UNIQUE)");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cadastrar(Aluno aluno) {
+        System.out.println("SALVANDO NO BANCO");  //teste para ver se o h2 está realmente funcionando
+
+        String sql = "INSERT INTO aluno (nome, cpf) VALUES (?, ?)";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, aluno.getNome());
+            stmt.setString(2, aluno.getCpf());
+            stmt.execute();
+
+        } catch (Exception e) {
+            System.out.println("Erro ao cadastrar: " + e.getMessage());
+        }
+    }
+
+    public List<Aluno> listar() {
+        List<Aluno> alunos = new ArrayList<>();
+        String sql = "SELECT nome, cpf FROM aluno";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                alunos.add(new Aluno(
+                        rs.getString("nome"),
+                        rs.getString("cpf")
+                ));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return alunos;
     }
+
     public void deletartodos() {
-        alunos.clear();
+        String sql = "DELETE FROM aluno";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             Statement stmt = conn.createStatement()) {
+
+            stmt.execute(sql);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }

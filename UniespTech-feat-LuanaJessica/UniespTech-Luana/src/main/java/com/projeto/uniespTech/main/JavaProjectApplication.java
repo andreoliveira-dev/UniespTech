99f92a60269098945b22bc9094f15870a8ff3927
luana <luana.jessica2018@gmail.com>
2026-03-29 23:09:34 -0300

    fix: padroniza package e corrige main class

--- UniespTech-feat-LuanaJessica/UniespTech-Luana/src/main/java/com/projeto/uniespTech/main/JavaProjectApplication.java
+++ UniespTech-feat-LuanaJessica/UniespTech-Luana/src/main/java/com/projeto/uniespTech/main/JavaProjectApplication.java
@@ -1,4 +1,4 @@
-package com.Projeto.UniespTech.main;
+package com.projeto.uniespTech.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
